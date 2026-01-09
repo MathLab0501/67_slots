56d09b63b93ba7aac284ec32f63ebd6706600581
MathLab0501 <mathieu.labbe.4@ulaval.ca>
2026-01-09 12:48:29 -0500

    UI mostly done, time to start slotsLogic.js

--- slot-machine/src/App.js
+++ slot-machine/src/App.js
@@ -1,22 +1,21 @@
 import './App.css';
-//import {setAmount} from './slotsLogic';
+import sixImg from './6.png';
+import sevenImg from './7.png';
+import poissonImg from './Poisson.png';
+import {spinReels}  from './slotsLogic';
 import { useState } from 'react';
 
 function App() {
   const [amount, setAmount] = useState(0);
   const [selectedButton, setSelectedButton] = useState(null);
-  const [wallet, setWallet] = useState(1000); // Hard coded 1000 for now, change to user wallet later
   const [gameStarted, setGameStarted] = useState(false);
   const [showError, setShowError] = useState(false);
-  console.log(amount);
+  const [showBlur, setShowBlur] = useState(true);
+  const [Reels, setReels] = useState([1,1,1,1]);
 
   const handleBetClick = (newAmount) => {
     setAmount(newAmount);
     setSelectedButton(newAmount);
-  };
-  const handleAllIn = () => {
-    setAmount(wallet);
-    setSelectedButton('all-in');  // Special marker for all-in
   };
 
   const handleGameStarted = () => {
@@ -24,48 +23,83 @@ function App() {
       setShowError(true);
     } else {
       setGameStarted(true);
+      setShowBlur(false);
       }
   }
+  const handleReels = () => {
+    const result = spinReels();
+    setReels(result);
+  }
 
-  return (
+  return ( //blur not yet working
       <div className="App">
+        <div className={`background ${showBlur ? "blur": ""}`}></div>
         {!gameStarted ? (
         <div className="login-container">
-          <input type="text" placeholder="Username" />
-          <h2 id="welcome"> Select your amount!</h2>
-          <div className="amount-buttons">
-            <button className={`amount-button ${selectedButton === 10 ? "selected" : ""}`} onClick={() => handleBetClick(10)}>10</button>
-            <button className={`amount-button ${selectedButton === 25 ? "selected" : ""}`} onClick={() => handleBetClick(25)}>25</button>
-            <button className={`amount-button ${selectedButton === 50 ? "selected" : ""}`} onClick={() => handleBetClick(50)}>50</button>
-            <button className={`amount-button ${selectedButton === 100 ? "selected" : ""}`} onClick={() => handleBetClick(100)}>100</button>
-            <button className={`amount-button ${selectedButton === 200 ? "selected" : ""}`} onClick={() => handleBetClick(200)}>200</button>
-            <button className={`amount-button ${selectedButton === 500 ? "selected" : ""}`} onClick={() => handleBetClick(500)}>500</button>
-            <button className={`amount-button ${selectedButton === "all-in" ? "selected" : ""}`} onClick={() => handleAllIn()}>ALL-IN</button>
+          <div className={"Title"}>
+            <img id={"casinoSix"} src={sixImg} alt="6" />
+            <h1 id={"title"}>Slots</h1>
+            <img id="casinoSeven"src={sevenImg} alt="7" />
           </div>
-          <h3>Current Bet: ${amount}</h3>
+          <div id={"username-container"}>
+            <img id={"fish1"} src={poissonImg} alt={"Spinning Fish"} />
+            <h2 className="username-text">Username:</h2>
+            <input id={"username_input"} type="text" placeholder="Username" />
+            <img id={"fish2"} src={poissonImg} alt={"Spinning Fish"} />
+          </div>
+          <button id="start" type="submit" onClick={() => handleGameStarted()}>Start</button>
+          <h2 id="welcome"> Select your amount!</h2>
 
           {showError && (
               <div className="error">
                 <p>Please select an amount to play</p>
               </div>
           )}
-          <button id="start" type="submit" onClick={() => handleGameStarted()}>Start</button>
         </div>
         )
       :
             (
         <div className="slots-container">
-          <div className="amount-buttons">
-            <button className={`amount-button ${selectedButton === 10 ? "selected" : ""}`} onClick={() => handleBetClick(10)}>10</button>
-            <button className={`amount-button ${selectedButton === 25 ? "selected" : ""}`} onClick={() => handleBetClick(25)}>25</button>
-            <button className={`amount-button ${selectedButton === 50 ? "selected" : ""}`} onClick={() => handleBetClick(50)}>50</button>
-            <button className={`amount-button ${selectedButton === 100 ? "selected" : ""}`} onClick={() => handleBetClick(100)}>100</button>
-            <button className={`amount-button ${selectedButton === 200 ? "selected" : ""}`} onClick={() => handleBetClick(200)}>200</button>
-            <button className={`amount-button ${selectedButton === 500 ? "selected" : ""}`} onClick={() => handleBetClick(500)}>500</button>
-            <button className={`amount-button ${selectedButton === "all-in" ? "selected" : ""}`} onClick={() => handleAllIn()}>ALL-IN</button>
+          <button id={"instructions"}>Instructions</button>
+          <div id={"robot"}>
+            <div id={"robot-head"}>
+              <div id="robot-face">
+                <div className='robot-eye' />
+                <div className='robot-eye' />
+              </div>
+              <div id={"robot-mouth"} />
+            </div>
+
+            <div id={"robot-body"}>
+              <div id={"robot-l-arm"}>
+                <div id={"robot-left-arm"}></div>
+                <div id={"robot-left-hand"}></div>
+              </div>
+              <div id={"robot-torso"}>
+                <div id={"robot-reels"}>
+                  <div className={"reel"}>{Reels[0]}</div>
+                  <div className={"reel"}>{Reels[1]}</div>
+                  <div className={"reel"}>{Reels[2]}</div>
+                  <div className={"reel"}>{Reels[3]}</div>
+                </div>
+              </div>
+              <div id={"robot-r-arm"}>
+                <div id={"robot-right-arm"}></div>
+                <div id={"robot-right-hand"}></div>
+              </div>
+            </div>
           </div>
+          <button id={"spin-button"} onClick={handleReels}>Spin</button>
         </div>
           )}
+        <div className="amount-buttons">
+          <button className={`amount-button ${selectedButton === 10 ? "selected" : ""}`} onClick={() => handleBetClick(10)}>10</button>
+          <button className={`amount-button ${selectedButton === 25 ? "selected" : ""}`} onClick={() => handleBetClick(25)}>25</button>
+          <button className={`amount-button ${selectedButton === 50 ? "selected" : ""}`} onClick={() => handleBetClick(50)}>50</button>
+          <button className={`amount-button ${selectedButton === 100 ? "selected" : ""}`} onClick={() => handleBetClick(100)}>100</button>
+          <button className={`amount-button ${selectedButton === 200 ? "selected" : ""}`} onClick={() => handleBetClick(200)}>200</button>
+          <button className={`amount-button ${selectedButton === 500 ? "selected" : ""}`} onClick={() => handleBetClick(500)}>500</button>
+        </div>
       </div>
   );
   }
---
+++ slot-machine/src/slotsLogic.js
@@ -0,0 +1,10 @@
+export function spinReels() {
+    return [
+        Math.ceil(Math.random() * 7),
+        Math.ceil(Math.random() * 7),
+        Math.ceil(Math.random() * 7),
+        Math.ceil(Math.random() * 7),
+    ]
+}
+
+export function spin() {}
